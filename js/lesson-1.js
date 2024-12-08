1b10621d5b6ac1cfb29358da72f268a3be9bfba2
Larimar4you <Zajanezlaja@gmail.com>
2024-12-08 18:45:04 +0100

    git push

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -1,7 +1,7 @@
 //! У змінній min лежить число від 0 до 59. Визначте у котру чверть години
 //! Попадає це число (у першу, другу, третю або четверту).
 
-function getQuoter(min) {
+/*function getQuoter(min) {
   if (min >= 0 && min < 15) {
     return 'first quater';
   } else if (min >= 15 && min < 30) {
@@ -14,4 +14,41 @@ function getQuoter(min) {
     return 'incorect';
   }
 }
-console.log(getQuoter(13));
+console.log(getQuoter(13));*/
+
+/*function getSizen(num = 3); {
+let result;
+switch (num) {
+  case 1:
+    result = "зима";
+    break;
+  case 2:
+    result = "весна";
+    break;
+  case 3:
+    result = "літо";
+    break;
+  case 4:
+    result = "осінь";
+    break;
+    default:
+    result = "Невірне значення";
+}
+return = result;
+}
+console.log(getSizen(3));*/
+
+/*//Напишіть функцію min(a, b), яка повертає
+//менше з чисел a, b
+//Потрібно додати перевірку, що функція отримує числа*/
+
+`use strickt`
+function min(a, b) {
+
+  if (typeof a !== "number" || typeof b !== "number" ) {
+    return `не число`;
+  }
+  return a < b ? a : b;
+}
+ console.log (min(4, "7"));
+ console.log (3, 4);
